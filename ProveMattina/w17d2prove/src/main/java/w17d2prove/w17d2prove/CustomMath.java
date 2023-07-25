f79a917226d40b2f02eed7b125ae056b72f70845
Walter Barbieri <walter90@hotmail.it>
2023-07-25 14:08:59 +0200

    Pranzo

---
+++ ProveMattina/w17d2prove/src/main/java/w17d2prove/w17d2prove/CustomMath.java
@@ -0,0 +1,15 @@
+package w17d2prove.w17d2prove;
+
+public class CustomMath {
+	public int sum(int a, int b) {
+		return a + b;
+	}
+
+	public int sum(int[] numbers) {
+		int sum = 0;
+		for (int i = 0; i < numbers.length; i++) {
+			sum += 1;
+		}
+		return sum;
+	}
+}
---
+++ ProveMattina/w17d2prove/src/test/java/w17d2prove/w17d2prove/CustomMathTests.java
@@ -0,0 +1,16 @@
+package w17d2prove.w17d2prove;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CustomMathTests {
+
+	@Test
+	void testSum() {
+		log.info("Test1");
+		assertTrue();
+	}
+}
